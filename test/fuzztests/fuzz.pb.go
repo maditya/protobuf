8cecbdf2c7179bfb66e529bcf5ec4161651f0c2b
Walter Schulze <awalterschulze@gmail.com>
2016-07-19 13:20:57 +0200

    merged ba6f978a1a6606adf3ccb6987f15c64262bfdbc2 from golang/protobuf

--- test/fuzztests/fuzz.pb.go
+++ test/fuzztests/fuzz.pb.go
@@ -2899,6 +2899,8 @@ var (
 	ErrIntOverflowFuzz   = fmt.Errorf("proto: integer overflow")
 )
 
+func init() { proto.RegisterFile("fuzz.proto", fileDescriptorFuzz) }
+
 var fileDescriptorFuzz = []byte{
 	// 444 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xac, 0xd3, 0xb1, 0x6e, 0xda, 0x50,
